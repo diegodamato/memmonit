535874c7cb017e49c55fc7057edc04698014e8ca
Diego Damato <diego.damato@b2wdigital.com>
2020-04-18 21:22:07 -0300

    Finalizando implementação de gravação de monitoramento em arquivo

--- test/RegisterMonitoring.spec.js
+++ test/RegisterMonitoring.spec.js
@@ -1,10 +1,10 @@
-let { assert } = require('chai');
-let { expect } = require('chai');
-// const FileHelper = require('./../lib/helper/FileHelper');
+const { assert, expect } = require('chai');
 const fs = require('fs');
 const util = require('util');
 const readFile = util.promisify(fs.readFile);
 const deleteFile = util.promisify(fs.unlink);
+const InvalidUnityError = require('./../lib/errors/InvalidUnityError');
+
 
 let registerMonitoring = require('./../lib/RegisterMonitoring');
 
@@ -12,23 +12,52 @@ describe('Testing the RegisterMonitoring class', () => {
     
     after(async() => {
         await deleteFile("./testeUsage.txt");
-        // FileHelper.deleteFile("./testeUsageTrunc");
-        // FileHelper.deleteFile("./testeUsageFixed");
+        await deleteFile("./testeUsageTrunc.txt");
+        await deleteFile("./testeUsageFixed.txt");
     })
 
-    it.only('#Testing getMemoryUsage by passing wrong unit by parameter(GBB)', async() => {
-        registerMonitoring.memoryUsage("./testeUsage.txt", "GB");
-        testeUsage = await readFile("./testeUsage.txt");
-        
-        assert.isString(testeUsage.toString())
-         
-        // expect(() => memoryMonitoring.getMemoryUsage("GBB")).to.throw(Error);
+    it('#Testing save monitoring of memoryUsage method to file', async() => {
+        await registerMonitoring.memoryUsage("./testeUsage.txt", "GB");
+        let file = await readFile("./testeUsage.txt");
+        let textFile = file.toString();
+
+        assert.isString(textFile)
+        assert.isTrue(textFile.includes("usage"));
+        assert.isTrue(textFile.includes("memory"));
     });
 
-    // it('#Testing getMemoryUsage by passing correct unit by parameter(GB)', () => {
-    //     assert.isNumber(memoryMonitoring.getMemoryUsage("GB"));
-    //     assert.isNumber(memoryMonitoring.getMemoryUsage("Gb"));
-    // });
+    it('#Testing save monitoring of memoryUsageTrunc method to file', async() => {
+        await registerMonitoring.memoryUsageTrunc("./testeUsageTrunc.txt", "GB");
+        let file = await readFile("./testeUsageTrunc.txt");
+        let textFile = file.toString();
 
+        assert.isString(textFile)
+        assert.isTrue(textFile.includes("usage"));
+        assert.isTrue(textFile.includes("memory"));
+    });
+
+    it('#Testing save monitoring of memoryUsageFixed method to file', async() => {
+        await registerMonitoring.memoryUsageFixed("./testeUsageFixed.txt", "GB");
+        let file = await readFile("./testeUsageFixed.txt");
+        let textFile = file.toString();
+
+        assert.isString(textFile)
+        assert.isTrue(textFile.includes("usage"));
+        assert.isTrue(textFile.includes("memory"));
+    });
+
+    it('#Testing save with error monitoring the memoryUsage method on file', () => {
+        expect(async() => await registerMonitoring.memoryUsage("./testeUsage.txt", "GBB").to.throw(InvalidUnityError));
+    });
+
+    it('#Testing save with error monitoring the memoryUsageTrunc method on file', () => {
+        expect(async() => await registerMonitoring.memoryUsageTrunc("./testeUsageTrunc.txt", "GBB").to.throw(InvalidUnityError));
+    });
+
+    it('#Testing save with error monitoring the memoryUsageFixed method on file', () => {
+        expect(async() => await registerMonitoring.memoryUsageFixed("./testeUsageFixes.txt", "GBB").to.throw(InvalidUnityError));
+    });
+
+   
 });
 
